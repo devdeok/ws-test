8bddbd6f4df537664bfb2e57d9ec09d210538c86
devdeok <dlwoejr700@gmail.com>
2022-08-10 16:42:20 +0900

    initial commit

---
+++ src/main/java/tmax/roger/wstest/domain/Chat.java
@@ -0,0 +1,20 @@
+package tmax.roger.wstest.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Chat {
+    @Id
+
+    private Long id;
+
+
+}
---
+++ src/test/java/tmax/roger/wstest/WsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tmax.roger.wstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

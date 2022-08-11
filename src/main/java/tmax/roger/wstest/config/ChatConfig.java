f1ffe1390cf3c65f3eec1e6fb74d91c7da301dd4
jaedeok <dlwoejr700@gmail.com>
2022-08-12 02:17:20 +0900

    commit at home

---
+++ src/main/java/tmax/roger/wstest/config/ChatConfig.java
@@ -0,0 +1,26 @@
+package tmax.roger.wstest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS(); // ws test client 접속 안됨
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.enableSimpleBroker("/queue", "/topic");
+
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
